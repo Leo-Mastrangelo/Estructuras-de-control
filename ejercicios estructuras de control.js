d31e384829e830a6bb20ef2e91804f9fd84a7f47
Leo-Mastrangelo <109768179+Leo-Mastrangelo@users.noreply.github.com>
2023-02-20 14:17:23 -0300

    Add files via upload

---
+++ ejercicios estructuras de control.js
@@ -0,0 +1,28 @@
+//factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+let i = 10
+for (i-1; i >= 0; i--){
+    console.log (i)
+}
+//bien hecho pero no lo carga
+
+//- factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+let r = 10
+while (r>=0){
+    console.log (r);
+    r--
+}
+//bien hecho pero no lo carga
+
+//- factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while,
+//una bifurcación if y una sentencia break
+
+let a = 10
+while (a>=0){
+    console.log (a)
+    a--
+    if (a>=5)
+    console.log (a)
+    break
+}
+//bien hecho pero no lo carga
